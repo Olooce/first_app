80e86c1b6bde12564c7a65ba96d73b0c43d9aee7
Olooce <asirastephen9@gmail.com>
2024-07-10 17:40:00 +0300

    refactor: optimize code

---
+++ lib/dice_roller.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return Image.asset('assets/images/dice-1.png', width: 234);
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller>{
+  @override
+Widget build(context){
+  return 
+}
+}
