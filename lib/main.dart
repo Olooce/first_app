363cb42b6faa646e7302d5299be7dba0d2e6515c
Olooce <asirastephen9@gmail.com>
2024-07-10 15:30:00 +0300

    refactor: optimize code

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     MaterialApp(
       home: Scaffold(
         // backgroundColor: Color.fromARGB(153, 0, 0, 0),
-        body:
+        body: GradientContainer(),
       ),
     ),
   );
@@ -16,22 +16,22 @@ class GradientContainer extends StatelessWidget {
   Widget build(context) {
     // TODO: implement build
     return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 2, 2, 56),
-                Color.fromARGB(197, 245, 140, 4)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello World!",
-              style: TextStyle(color: Colors.white, fontSize: 28.3),
-            ),
-          ),
-        )
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 2, 2, 56),
+            Color.fromARGB(197, 245, 140, 4)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello World!",
+          style: TextStyle(color: Colors.white, fontSize: 28.3),
+        ),
+      ),
+    );
   }
 }
