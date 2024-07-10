1618ac9030d75c463404634739c2585813affbbd
Olooce <asirastephen9@gmail.com>
2024-07-10 16:40:00 +0300

    refactor: optimize code

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
       home: Scaffold(
         // backgroundColor: Color.fromARGB(153, 0, 0, 0),
         body: GradientContainer(
-            colors: [Color.fromARGB(186, 207, 66, 10), Colors.grey, Color.fromARGB(255, 224, 130, 7), Color.fromARGB(255, 240, 243, 33)]),
+            colors: [Color.fromARGB(186, 207, 66, 10), Color.fromARGB(255, 179, 64, 19), Color.fromARGB(255, 224, 130, 7), Color.fromARGB(255, 240, 243, 33)]),
       ),
     ),
   );
