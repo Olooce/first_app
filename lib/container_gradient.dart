b8bd9f06ee7bf7f49dbd839af434947353d9437e
Olooce <asirastephen9@gmail.com>
2024-07-10 16:30:00 +0300

    style: improve code formatting

--- lib/container_gradient.dart
+++ lib/container_gradient.dart
@@ -2,17 +2,16 @@ import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 2, 2, 56),
-            Color.fromARGB(197, 245, 140, 4)
-          ],
+          colors: colors,
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ void main() {
     const MaterialApp(
       home: Scaffold(
         // backgroundColor: Color.fromARGB(153, 0, 0, 0),
-        body: GradientContainer(),
+        body: GradientContainer(
+            colors: [Color.fromARGB(186, 207, 66, 10), Colors.grey, Color.fromARGB(255, 224, 130, 7), Color.fromARGB(255, 240, 243, 33)]),
       ),
     ),
   );
